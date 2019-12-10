2138f5433faba53464235cfaa7e592f61cec3ef3
mxc035 <malcolm.crum@ioof.com.au>
2019-12-10 16:58:01 +1100

    Progress... but tests still failing

--- python/book-store/book_store.py
+++ python/book-store/book_store.py
@@ -1,16 +1,36 @@
 from collections import Counter
+from typing import List, Set
 
-def min_price(counts):
-    count = counts[1]
-    if (count == 0):
-      return counts[1:]
+book_price = 8
+discounts = {1: 100, 2: 95, 3: 90, 4: 80, 5: 75}
+
+
+def min_price(books: List[int], books_in_set: List[int]) -> float:
+    if len(books) == 0:
+        return cost(books_in_set)
+    if set(books).issubset(set(books_in_set)):  # no more unique books to add to the set
+        return cost(books_in_set) + min_price(books, [])
+    book: int = books[0]
+    remaining_books: List[int] = books[1:]
+    if book in books_in_set:
+        print(f"already have {book} in set {books_in_set}. remaining: {remaining_books}")
+        return min_price(remaining_books + [book], books_in_set)  # put book at end to pick up later
     else:
-      a = 1 + [counts[1] - 1] + counts[1:]
-      b = counts[1:]
+        continue_set = min_price(remaining_books, books_in_set + [book])
+        end_set = cost(books_in_set + [book]) + min_price(remaining_books, [])
+        if continue_set < end_set:
+            return continue_set
+        else:
+            print(f"found cheap set: {books_in_set} costs {end_set}, remaining: {books}")
+            return end_set
 
 
-def total(basket):
-    counts = list(Counter(basket).values())
-    return min_price(counts)
+def cost(books: List[int]) -> float:
+    if len(books) == 0:
+        return 0
+    discount = discounts[len(books)]
+    return len(books) * book_price * discount
 
-assert total([1]) == 8
+
+def total(basket: List[int]) -> float:
+    return min_price(basket, [])
