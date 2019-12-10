9fdfa621e8990a6034c2cd68bde4a834a4d8b0b7
mxc035 <malcolm.crum@ioof.com.au>
2019-12-10 17:08:12 +1100

    Progress... but tests still failing

--- python/book-store/book_store.py
+++ python/book-store/book_store.py
@@ -6,22 +6,25 @@ discounts = {1: 100, 2: 95, 3: 90, 4: 80, 5: 75}
 
 
 def min_price(books: List[int], books_in_set: List[int]) -> float:
-    if len(books) == 0:
-        return cost(books_in_set)
+    assert len(books_in_set) == len(set(books_in_set))
+    if len(books) == 0:  # no more books to calculate
+        value = cost(books_in_set)
+        return value
+
     if set(books).issubset(set(books_in_set)):  # no more unique books to add to the set
         return cost(books_in_set) + min_price(books, [])
+
     book: int = books[0]
     remaining_books: List[int] = books[1:]
-    if book in books_in_set:
-        print(f"already have {book} in set {books_in_set}. remaining: {remaining_books}")
-        return min_price(remaining_books + [book], books_in_set)  # put book at end to pick up later
+
+    if book in books_in_set:  # skip book, put at end of list to pick up later
+        return min_price(remaining_books + [book], books_in_set)
     else:
         continue_set = min_price(remaining_books, books_in_set + [book])
         end_set = cost(books_in_set + [book]) + min_price(remaining_books, [])
         if continue_set < end_set:
             return continue_set
         else:
-            print(f"found cheap set: {books_in_set} costs {end_set}, remaining: {books}")
             return end_set
 
 
@@ -29,7 +32,9 @@ def cost(books: List[int]) -> float:
     if len(books) == 0:
         return 0
     discount = discounts[len(books)]
-    return len(books) * book_price * discount
+    value = len(books) * book_price * discount
+    print(f"discount {discount} for {books}: ${value}")
+    return value
 
 
 def total(basket: List[int]) -> float:
